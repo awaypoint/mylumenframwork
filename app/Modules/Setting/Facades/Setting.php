689aa958d519381fe6f9e7a4eb7e7186a3061244
amoj <215089670@qq.com>
2018-04-08 22:58:07 +0800

    1 gas 2 water

--- app/Modules/Setting/Facades/Setting.php
+++ app/Modules/Setting/Facades/Setting.php
@@ -5,9 +5,9 @@ use Illuminate\Support\Facades\Facade;
 
 class Setting extends Facade
 {
-    const SETTING_WASTE_TYPE_MAP = ['1' => '废水', '2' => '废气'];
-    const SETTING_WASTE_WATER_TYPE = 1;
-    const SETTING_WASTE_GAS_TYPE = 2;
+    const SETTING_WASTE_TYPE_MAP = ['1' => '废气', '2' => '废水'];
+    const SETTING_WASTE_WATER_TYPE = 2;
+    const SETTING_WASTE_GAS_TYPE = 1;
 
 	protected static function getFacadeAccessor(){
 		return 'setting';
